576b79bc9a135378f1189df6494b6cf1b4f0642c
tagtribble <tagtribble@gmail.com>
2026-05-27 12:23:25 -0400

    Added self installazation (I dont feel like spelling)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,60 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <climits>
+#include "unistd.h"
 #include "agents.h"
 
+int setup(){
+    std::cout << "Starting install" << std::endl;
+
+    const std::string target_dir =  "/usr/local/bin/";
+
+    // Check current directory
+    char result[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    std::string absolute_binary_path = (count != -1) ? std::string(result, count) : "";
+
+    // Move self to target dir
+    std::string command = "sudo cp \"" + absolute_binary_path + "\" \"" + target_dir + "wn" + "\" && sudo chmod +x \"" + target_dir + "wn" + "\"";
+    int bin_result = system(command.c_str());
+    if (bin_result != 0) {
+        return 1;
+    }
+
+    // Create storage files
+    std::string main_folder_cmd = "mkdir ~/.wrens_nest/";
+    std::string temp_folder_cmd = "mkdir ~/.wrens_nest/temp/";
+    std::string data_folder_cmd = "mkdir ~/.wrens_nest/data/";
+
+    int main_result = system(main_folder_cmd.c_str());
+    int temp_result = system(temp_folder_cmd.c_str());
+    int data_result = system(data_folder_cmd.c_str());
+
+    return 0;
+}
+
+int verify_setup(std::string file_path){
+    std::ifstream f(file_path.c_str());
+    if (!f.good()){
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
+    std::string test_folder = "/usr/local/bin/wn";
+    if (verify_setup(test_folder) == 1){
+        if (setup() != 0) {
+            std::cout << "Failed to setup program, aborting" << std::endl;
+        } else {
+            std::cout << "Installation finished, program is ready." << std::endl;
+        }
+    } else {
+        std::cout << "Installation verrifyed" << std::endl;
+    }
+
     std::string response;
 
     if (argc > 1){
